6ae5d3bf57b5011e7e904c4ce2515ed84c6da006
Peter Vermeulen <contact@valkyriestudios.be>
2025-05-08 21:42:49 +0200

    [feat] Add filename encoder utility

--- lib/utils/String.ts
+++ lib/utils/String.ts
@@ -78,3 +78,46 @@ export function hexId (lng:number): string {
         }
     }
 }
+
+/**
+ * Encodes a filename for safe usage (in for example headers such as Content-Disposition)
+ * eg:
+ * encodeFilename('Straße_(draft)*v1.0.pdf')
+ * → {
+ *   ascii: 'Strasse_(draft)*v1.0.pdf',
+ *   encoded: 'Stra%C3%9Fe_%28draft%29%2Av1.0.pdf'
+ * }
+ * 
+ * @param {string} val - Name to encode
+ */
+export function encodeFilename (val:string):{ascii:string; encoded:string} {
+    let ascii = '';
+    let encoded = '';
+    for (let i = 0; i < val.length; i++) {
+        const char = val[i];
+
+        /* Skip newlines and control chars */
+        const code = char.charCodeAt(0);
+        if (code < 0x20 || code === 0x7F || char === '"' || char === '\\') continue;
+
+        /* ASCII fallback */
+        ascii += code <= 0x7E ? char : '';
+
+        /* EncodeURIComponent with patched chars */
+        switch (char) {
+            case '*':
+                encoded += '%2A';
+                break;
+            case '\'':
+            case '(':
+            case ')':
+                encoded += '%' + code.toString(16).toUpperCase();
+                break;
+            default:
+                encoded += encodeURIComponent(char);
+                break;
+        }
+    }
+
+    return {ascii, encoded};
+}
