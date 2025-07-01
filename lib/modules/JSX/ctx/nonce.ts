a251981570adc1af0501dea951e58b60c83fc5af
Peter Vermeulen <contact@valkyriestudios.be>
2025-07-01 11:16:06 +0200

    [feat] Add nonce win script

--- lib/modules/JSX/ctx/nonce.ts
+++ lib/modules/JSX/ctx/nonce.ts
@@ -1,5 +1,24 @@
 import {getActiveCtx} from '../ctx/use';
 
+export const NONCEMARKER = 'tfnonce';
+
 export function nonce(): string | null {
     return getActiveCtx()?.nonce || null;
+}
+
+/**
+ * Nonce Window script setter for full-page ssr behavior
+ *
+ * @param {string} val - Nonce value
+ */
+export function NONCE_WIN_SCRIPT(val: string) {
+    if (typeof val !== 'string' || !val.length) return '';
+    return [
+        '<script nonce="' + val + '">',
+        'Object.defineProperty(window,"$' + NONCEMARKER + '",{',
+        'value:"' + val + '",',
+        'configurable:!1,',
+        'writable:!1',
+        '})</script>',
+    ].join('');
 }
--- test/unit/modules/JSX/ctx/nonce.test.ts
+++ test/unit/modules/JSX/ctx/nonce.test.ts
@@ -1,24 +1,41 @@
 import {describe, it, expect, beforeEach} from 'vitest';
-import {nonce} from '../../../../../lib/modules/JSX/ctx/nonce';
+import {nonce, NONCE_WIN_SCRIPT} from '../../../../../lib/modules/JSX/ctx/nonce';
 import {setActiveCtx} from '../../../../../lib/modules/JSX/ctx/use';
 import {MockContext} from '../../../../MockContext';
+import CONSTANTS from '../../../../constants';
 
-describe('JSX - ctx/nonce', () => {
-    beforeEach(() => {
-        setActiveCtx(null);
+describe('Modules - JSX - ctx - nonce', () => {
+    describe('nonce', () => {
+        beforeEach(() => {
+            setActiveCtx(null);
+        });
+
+        it('Returns null if no context is active', () => {
+            expect(nonce()).toBeNull();
+        });
+
+        it('Returns nonce from context if set', () => {
+            setActiveCtx(new MockContext({nonce: 'abc123'}));
+            expect(nonce()).toBe('abc123');
+        });
+
+        it('Returns null if context is active but nonce is missing', () => {
+            setActiveCtx(new MockContext({nonce: null}));
+            expect(nonce()).toBe(null);
+        });
     });
 
-    it('Returns null if no context is active', () => {
-        expect(nonce()).toBeNull();
-    });
+    describe('NONCE_WIN_SCRIPT', () => {
+        it('Should return a minified script containing the provided value', () => {
+            expect(NONCE_WIN_SCRIPT('abc123')).toBe(
+                '<script nonce="abc123">Object.defineProperty(window,"$tfnonce",{value:"abc123",configurable:!1,writable:!1})</script>',
+            );
+        });
 
-    it('Returns nonce from context if set', () => {
-        setActiveCtx(new MockContext({nonce: 'abc123'}));
-        expect(nonce()).toBe('abc123');
-    });
-
-    it('Returns null if context is active but nonce is missing', () => {
-        setActiveCtx(new MockContext({nonce: null}));
-        expect(nonce()).toBe(null);
+        it('Should return an empty string if passed a non-string or empty nonce', () => {
+            for (const el of [...CONSTANTS.NOT_STRING, '']) {
+                expect(NONCE_WIN_SCRIPT(el as any)).toBe('');
+            }
+        });
     });
 });
